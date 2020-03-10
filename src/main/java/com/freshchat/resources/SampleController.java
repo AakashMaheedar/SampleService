cf49c0f98c494c1bd49294819eae0028641087b6
Aakash Maheedar <58288773+Aakash-Maheedar@users.noreply.github.com>
2020-03-11 00:40:18 +0530

    initial commit -setup with .sh script

---
+++ src/main/java/com/freshchat/resources/SampleController.java
@@ -0,0 +1,21 @@
+package com.freshchat.resources;
+
+import com.freshchat.domain.Sample;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class SampleController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Sample greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Sample(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/main/java/com/freshchat/service/SampleService.java
@@ -0,0 +1,14 @@
+package com.freshchat.service;
+
+import com.freshchat.domain.Sample;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class SampleService {
+
+    private static final String template = "Hello, %s!";
+
+}
